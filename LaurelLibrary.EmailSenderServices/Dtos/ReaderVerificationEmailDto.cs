fcf7facf34c5906ee589ce1df250803882bdb59c
Aliaksei Aksionau <alexei.aksenov@gmail.com>
2025-10-23 17:01:58 -0500

    Move EmailSenderServices to nuget package

--- LaurelLibrary.EmailSenderServices/Dtos/ReaderVerificationEmailDto.cs
+++
@@ -1,7 +0,0 @@
-namespace LaurelLibrary.EmailSenderServices.Dtos;
-
-public class ReaderVerificationEmailDto
-{
-    public string ReaderName { get; set; } = string.Empty;
-    public string VerificationCode { get; set; } = string.Empty;
-}
